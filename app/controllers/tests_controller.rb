55fc28be79546293655c9782b33b299f998c48f0
Kristian Perez <kpere192@fiu.edu>
2024-05-28 12:16:03 -0400

    Added a very basic way to make new tests in new.html, and fixed old error where I was using title instead of name for tests

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,28 @@
 class TestsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
 
     def index
         @tests = Test.all
+    end
+
+    def new
+        @test = Test.new
+    end
+
+    def create
+        @user = current_user
+        @test = @user.created_tests.build(test_params)
+
+        if @test.save
+            redirect_to @test
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+
+    private
+    def test_params
+        params.require(:test).permit(:name, :description)
     end
 end
